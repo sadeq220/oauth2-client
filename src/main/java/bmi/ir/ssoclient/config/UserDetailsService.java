4c2ff7a4350c589e92356de716b53cf3f47d57ab
sadeq <sdqSafdari@gmail.com>
2024-03-02 14:29:18 +0330

    use Auth0 JWT library to decode and verify baam access token JWT

--- src/main/java/bmi/ir/ssoclient/config/UserDetailsService.java
+++ src/main/java/bmi/ir/ssoclient/config/UserDetailsService.java
@@ -1,11 +1,17 @@
 package bmi.ir.ssoclient.config;
 
+import bmi.ir.ssoclient.cryptography.SecretKeyReader;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
+
+import java.util.Base64;
 
 @Component
 /**
@@ -14,9 +20,20 @@ import org.springframework.stereotype.Component;
  * the result will be set in SecurityContextHolder
  */
 public class UserDetailsService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final SecretKeyReader secretKeyReader;
+
+    public UserDetailsService(SecretKeyReader secretKeyReader) {
+        this.secretKeyReader = secretKeyReader;
+    }
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2AccessToken accessToken = userRequest.getAccessToken();
+        OAuth2AccessToken accessToken = userRequest.getAccessToken();// baam access token is JWT
+        Algorithm algorithm = Algorithm.HMAC256(Base64.getEncoder().encode(secretKeyReader.getOAuth2AccessTokenSecretKey()));
+        DecodedJWT decodedJWT = JWT.decode(accessToken.getTokenValue());// auth0 SDK
+        algorithm.verify(decodedJWT);// only verify signature and ignore "exp" and "nbf" claims
+        String nationalId = decodedJWT.getClaims().get("ssn").asString();
+
         return null;
     }
 }
