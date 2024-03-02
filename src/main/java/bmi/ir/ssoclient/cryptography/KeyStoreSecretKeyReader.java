25c6713b5769be03ce48e448b2cb569df0ce5483
sadeq <sdqSafdari@gmail.com>
2024-03-02 11:41:03 +0330

    Implement secret key cache for KeyStoreSecretKeyReader

--- src/main/java/bmi/ir/ssoclient/cryptography/KeyStoreSecretKeyReader.java
+++ src/main/java/bmi/ir/ssoclient/cryptography/KeyStoreSecretKeyReader.java
@@ -10,44 +10,46 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.util.Map;
 
 @Component
 public class KeyStoreSecretKeyReader implements SecretKeyReader{
     private final String keyStorePassword;
     private final Resource keyStore;
+    private final Map<String,byte[]> secretKeyCache;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public KeyStoreSecretKeyReader(@Value("${keystore.password}")String keystorePassword,
-                                   @Value("${keystore.name}") String keyStoreName){
+                                   @Value("${keystore.name}") String keyStoreName) throws Exception{
         this.keyStorePassword = keystorePassword;
         this.keyStore = new ClassPathResource("keys/"+keyStoreName);
-
+        this.secretKeyCache = this.initCache();
     }
-    @Override
-    public byte[] getOAuth2AccessTokenSecretKey() {
+    private Map initCache() throws Exception {
         try {
             KeyStore pkcs12Keystore = KeyStore.getInstance("PKCS12");
             pkcs12Keystore.load(keyStore.getInputStream(),keyStorePassword.toCharArray());
-            Key baam_secret_key = pkcs12Keystore.getKey("baam_secret_key", keyStorePassword.toCharArray());
-            return baam_secret_key.getEncoded();
+            Key baam_secret_key = pkcs12Keystore.getKey("baam_jwt_secretkey", keyStorePassword.toCharArray());
+            return Map.ofEntries( // unmodifiable map
+                    Map.entry("baam_jwt_secretkey",baam_secret_key.getEncoded()));
         } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException e) {
             logger.error("keystore reading exception!",e);
-            throw this.secretKeyException(e);
+            throw e;
         } catch (IOException e) {
-            logger.error("keystore finding exception",e);
-            throw this.secretKeyException(e);
+            logger.error("keystore finding exception or PKCS12 integrity checking exception",e);
+            throw e;
         } catch (UnrecoverableKeyException e) {
             logger.error("specified key does not exist in keystore!",e);
-            throw this.secretKeyException(e);
+            throw e;
         }
+    }
+    @Override
+    public byte[] getOAuth2AccessTokenSecretKey() {
+        return secretKeyCache.get("baam_jwt_secretkey");
     }
 
     @Override
     public byte[] getOAuth2ClientSecretKey() {
         return new byte[0];
-    }
-
-    private RuntimeException secretKeyException(Exception cause){
-        return new RuntimeException("secret key reading exception",cause);
     }
 }
