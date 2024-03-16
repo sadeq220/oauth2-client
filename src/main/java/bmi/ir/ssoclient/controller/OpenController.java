a194dabf059bcaa4b515c879a279bf986bde7be2
sadeq <sdqSafdari@gmail.com>
2024-03-16 14:18:52 +0330

    update sample user-info api

--- src/main/java/bmi/ir/ssoclient/controller/OpenController.java
+++ src/main/java/bmi/ir/ssoclient/controller/OpenController.java
@@ -1,12 +1,14 @@
 package bmi.ir.ssoclient.controller;
 
+import bmi.ir.ssoclient.controller.model.UserInfoDto;
+import bmi.ir.ssoclient.userInfo.UserInfoAccessor;
+import bmi.ir.ssoclient.userInfo.model.UserInfoModel;
 import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Properties;
 
 @RestController
 @RequestMapping("/air")
@@ -14,11 +16,12 @@ public class OpenController {
     @Autowired
     @Qualifier("springSecurityFilterChain")
     private Filter filterChainProxy;// spring security FilterChainProxy
-    @RequestMapping("/user")
-    public Properties testFilterChain(){
-        Properties properties = new Properties();
-        properties.put("name","sadeq");
-        properties.put("age",27);
-        return properties;
+    @Autowired
+    private UserInfoAccessor userInfoAccessor;
+
+    @RequestMapping("/user-info")
+    public UserInfoDto testFilterChain(){
+        UserInfoModel identity = userInfoAccessor.getIdentity("0019440619");
+        return UserInfoDto.create(identity);
     }
 }
