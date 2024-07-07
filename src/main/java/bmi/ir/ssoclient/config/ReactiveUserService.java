ef9a47d4ef8d8847167c5225b222d63e62bc5ff5
sadeq <sdqSafdari@gmail.com>
2024-07-07 15:08:57 +0330

    make user service reactive

--- src/main/java/bmi/ir/ssoclient/config/ReactiveUserService.java
+++ src/main/java/bmi/ir/ssoclient/config/ReactiveUserService.java
@@ -8,34 +8,36 @@ import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 import java.util.Base64;
 
-//@Component
+@Component
 /**
- * automatically registered with OAuth2LoginAuthenticationProvider
+ * automatically registered with OAuth2LoginReactiveAuthenticationManager
+ * in reactive spring-security there is no ProviderManager or AuthenticationProvider counterpart. there is only ReactiveAuthenticationManager.
  * the input is result of tokenUri endpoint
  * the result will be set in SecurityContextHolder as a principal object
  */
-public class UserDetailsService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+public class ReactiveUserService implements ReactiveOAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final SecretKeyReader secretKeyReader;
     private final UserInfoAccessor userInfoAccessor;
     private final Logger logger= LoggerFactory.getLogger(this.getClass());
 
-    public UserDetailsService(SecretKeyReader secretKeyReader,
+    public ReactiveUserService(SecretKeyReader secretKeyReader,
                               UserInfoAccessor userInfoAccessor) {
         this.secretKeyReader = secretKeyReader;
         this.userInfoAccessor = userInfoAccessor;
     }
-
     @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+    public Mono<OAuth2User> loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2AccessToken accessToken = userRequest.getAccessToken();// baam access token is JWT
         Algorithm algorithm = Algorithm.HMAC256(Base64.getEncoder().encode(secretKeyReader.getOAuth2AccessTokenSecretKey()));
         DecodedJWT decodedJWT = JWT.decode(accessToken.getTokenValue());// auth0 SDK
@@ -46,10 +48,11 @@ public class UserDetailsService implements OAuth2UserService<OAuth2UserRequest, 
             throw new OAuth2AuthenticationException("access token signature invalid!");
         }
         try{
-        String nationalId = decodedJWT.getClaims().get("ssn").asString();
-        return userInfoAccessor.getIdentity(nationalId);
+            String nationalId = decodedJWT.getClaims().get("ssn").asString();
+            return Mono.just(userInfoAccessor.getIdentity(nationalId));
         } catch (Exception ex){
             throw new OAuth2AuthenticationException(ex.getMessage());
         }
     }
+
 }
