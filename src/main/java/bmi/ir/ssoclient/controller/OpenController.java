2979c0215801640ecb7eaba210ba553504f8bded
sadeq <sdqSafdari@gmail.com>
2024-07-08 14:05:54 +0330

    enhance csrf-token doc

--- src/main/java/bmi/ir/ssoclient/controller/OpenController.java
+++ src/main/java/bmi/ir/ssoclient/controller/OpenController.java
@@ -31,6 +31,8 @@ public class OpenController {
     /**
      * To protect against CSRF exploit backend services
      * safe Methods(GET, HEAD, OPTIONS, and TRACE) Must be Read-only.
+     *
+     *  Synchronizer Token Pattern(best practice) expects csrf-tokens be present in Header(or request param) of not safe requests.
      */
     @RequestMapping("/csrf-token")
     public Mono<Properties> getCsrfToken(ServerWebExchange exchange){
