f653c13d26b6ff33e35c417d41ac20633601fffb
sadeq <sdqSafdari@gmail.com>
2024-07-08 13:52:59 +0330

    make csrf-token endpoint reactive

--- src/main/java/bmi/ir/ssoclient/controller/OpenController.java
+++ src/main/java/bmi/ir/ssoclient/controller/OpenController.java
@@ -4,11 +4,12 @@ import bmi.ir.ssoclient.controller.model.UserInfoDto;
 import bmi.ir.ssoclient.userInfo.UserInfoAccessor;
 import bmi.ir.ssoclient.userInfo.model.UserInfoModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.server.WebFilterChainProxy;
+import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 import java.util.Properties;
 
@@ -22,16 +23,23 @@ public class OpenController {
     private UserInfoAccessor userInfoAccessor;
 
     @RequestMapping("/user-info")
-    public UserInfoDto testFilterChain(){
+    public Mono<UserInfoDto> testFilterChain(){
         UserInfoModel identity = userInfoAccessor.getIdentity("0019440619");
-        return UserInfoDto.create(identity);
+        return Mono.just(UserInfoDto.create(identity));
     }
-//    @RequestMapping("/csrf-token")
-//    public Properties getCsrfToken(HttpServletRequest request){
-//        CsrfToken csrf = (CsrfToken) request.getAttribute("_csrf");
-//        Properties properties = new Properties();
-//        properties.put("token",csrf.getToken());
-//        properties.put("headerName","X-CSRF-TOKEN");
-//        return properties;
-//    }
+
+    /**
+     * To protect against CSRF exploit backend services
+     * safe Methods(GET, HEAD, OPTIONS, and TRACE) Must be Read-only.
+     */
+    @RequestMapping("/csrf-token")
+    public Mono<Properties> getCsrfToken(ServerWebExchange exchange){
+        Mono<CsrfToken> csrfTokenMono = exchange.getAttribute(CsrfToken.class.getName());
+        return csrfTokenMono.map(csrfToken -> {
+            Properties properties = new Properties();
+            properties.put("token", csrfToken.getToken());
+            properties.put("headerName", "X-XSRF-TOKEN");
+            return properties;
+        });
+    }
 }
