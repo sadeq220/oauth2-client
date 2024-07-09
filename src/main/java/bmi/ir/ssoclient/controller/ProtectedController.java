dd89edfe81e7c71c0fe2da31b6bdafbf5fabb4b5
sadeq <sdqSafdari@gmail.com>
2024-07-09 15:33:29 +0330

    make user-info endpoint reactive

--- src/main/java/bmi/ir/ssoclient/controller/ProtectedController.java
+++ src/main/java/bmi/ir/ssoclient/controller/ProtectedController.java
@@ -3,19 +3,21 @@ package bmi.ir.ssoclient.controller;
 import bmi.ir.ssoclient.controller.model.UserInfoDto;
 import bmi.ir.ssoclient.userInfo.model.UserInfoModel;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/protected")
 public class ProtectedController {
 
     @GetMapping("/user-info")
-    public Object userInfo(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal();
-        return UserInfoDto.create((UserInfoModel) principal);
+    public Mono<UserInfoDto> userInfo(){
+        return ReactiveSecurityContextHolder.getContext()
+                .map(securityContext -> (UserInfoModel)securityContext.getAuthentication().getPrincipal())
+                .map(userInfoModel -> UserInfoDto.create(userInfoModel));
     }
 }
