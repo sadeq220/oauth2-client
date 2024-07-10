e2382f5a95f3959905a44b57fe92beee6316d738
sadeq <sdqSafdari@gmail.com>
2024-07-10 10:50:07 +0330

    revise MVCConfig.java name

--- src/main/java/bmi/ir/ssoclient/config/GeneralConfig.java
+++ src/main/java/bmi/ir/ssoclient/config/GeneralConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MVCConfig {
+public class GeneralConfig {
     @Bean
     public ObjectMapper jacksonMapper(){
         ObjectMapper objectMapper = new ObjectMapper();
